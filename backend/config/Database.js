9d500130bd5c898127dc71b18d4721fff9721931
nadyahrxa <nadyahrxa@gmail.com>
2024-10-16 12:18:21 +0700

    new commit

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,18 @@
-import sequelize from "sequelize";
+import { Sequelize } from "sequelize";
 
-const db = new sequelize('crud_db','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
+const db = new Sequelize('sql12738076', 'sql12738076', 'andVuAnmVF', {
+    host: 'sql12.freesqldatabase.com',
+    dialect: 'mysql',
+    port: 3306
 });
+
+// Uji koneksi
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 export default db;
